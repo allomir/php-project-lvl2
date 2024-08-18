b7713103875350e28ab9c0cbf2d6f08fe01114b5
allomir <alomir@yandex.ru>
2024-08-18 12:16:37 +0300

    convert structure to data-model

--- src/Functions/FS.php
+++ src/Functions/FS.php
@@ -4,22 +4,63 @@
 namespace Hexlet\Code\Functions\FS;
 
 /**
- * `filesystem(FS)
- * функция (функция с одним процесс-действие, функция простая)
- * Название: Получение путь абс из путь относительно PWD
- * - Путь относительный: ./ или начало без /
- *      преобразовать в абс
- * - Путь абс: возвращается то же значение
+ * функция свойства файла
+ * - путь абс
+ * - имя
+ * - расширение
+ * - размер файла
+ * Кроме:
+ * - стуркутура (Format)
+ *      работа со структурой выполняется в программах отдельных
+ *      работа с текстовые файлы (Text) выполняют функции DataFormats
  */
-function getFilePathnameByPWD(string $pathname): string
+function setFileModel($pathname)
 {
-    if (strpos($pathname, './') === 0) {
-        $pathname = $_SERVER['PWD'] . '/' . substr($pathname, 2);
-    } elseif (strpos($pathname, '/') !== 0) {
-        $pathname = $_SERVER['PWD'] . '/' . $pathname;
+    $characteristics = [
+        'pathname' => $pathname,
+        'name' => pathinfo($pathname)['basename'],
+        'extension' => pathinfo($pathname)['extension'],
+            // kind by Extention into a name
+            // для определения вида по структуре необходимо анализ код или подбор программы
+    ];
+
+    return $characteristics;
+}
+
+/**
+ * функция проверка-разрешение Файл характеристика расширение
+ * - $settingExtensions: настройка модуля разрешенные расширения
+ * - $fileExtension: только расширение, не указывается название файла в exception
+ */
+function permitFileExtension(array $settingExtensions, string $fileExtension): bool
+{
+    if (!in_array($fileExtension, $settingExtensions)) {
+        return false;
     }
 
-    return $pathname;
+    return true;
+}
+
+/**
+ * функция проверка-разрешение Файл характеристика расширение
+ * Предназначение: настройка программы данные пограничные условия 
+ * - $settingExtensions: настройка модуля разрешенные расширения
+ * - $file: все характеристики
+ *      - расширение
+ *      - название
+ */
+function permitFileExtension__model(array $settingExtensions, array $file)
+{
+    $fileExtension = $file['extension'];
+    $fileName = $file['name'];
+
+    if (!permitFileExtension($settingExtensions, $fileExtension)) {
+        throw new \exception(
+            'Файл расширение: проверка-разрешение. Error: ' . $fileName
+        );
+    }
+
+    return true;
 }
 
 /**
@@ -32,11 +73,46 @@ function getFilePathnameByPWD(string $pathname): string
  * - Путь Относительно project (невкл)
  *      возможна доработка
  */
-function getFilePathnameMainset(string $pathname): string
+function getFilePathnameSet(string $pathname): string
 {
-    $pathname = getFilePathnameByPWD($pathname);
+    $pathname__abs = getFilePathname__PWD($pathname);
 
     return $pathname;
+}
+
+/**
+ * `filesystem(FS)
+ * функция (функция с одним процесс-действие, функция простая)
+ * Название: Получение путь абс из путь относительно PWD
+ * - Путь относительный: ./ или начало без /
+ *      преобразовать в абс
+ * - Путь абс: возвращается то же значение
+ */
+function getFilePathname__PWD(string $pathname): string
+{
+    $pathname__abs = $pathname;
+
+    if (strpos($pathname, './') === 0) {
+        $pathname__abs = $_SERVER['PWD'] . '/' . substr($pathname, 2);
+    } elseif (strpos($pathname, '/') !== 0) {
+        $pathname__abs = $_SERVER['PWD'] . '/' . $pathname;
+    }
+
+    return $pathname__abs;
+}
+
+/**
+ * Функция-процессы (функция из несколько процесс-действие)
+ * Название: проверка характеристики файлов
+ * - существование (вкл)
+ */
+function checkFile__Pathname(string $pathname): bool
+{
+    if (!file_exists($pathname)) {
+        return false;
+    }
+
+    return true;
 }
 
 /**
@@ -45,12 +121,15 @@ function getFilePathnameMainset(string $pathname): string
  * - существование (вкл)
  * - соответствие структуры формата json (невкл)
  */
-
-function checkFileMainset(string $pathname): bool
+function checkFile__model(array $file): bool
 {
-    if (!file_exists($pathname)) {
-        throw new \exception('Error. файл путь: ' . $file1_pathname);
-        return false;
+    $filPathname = $file['pathname'];
+    $fileName = $file['name'];
+
+    if (!file_exists($filPathname)) {
+        throw new \exception(
+            'Файл путь: проверка. Error: ' . $fileName
+        );
     }
 
     return true;
--- src/gendiff.php
+++ src/gendiff.php
@@ -5,37 +5,63 @@ namespace Hexlet\Code\gendiff;
 ### Настройка проекта. namespace. настройка Composer autoload. список загрузки
 use function Hexlet\Code\Functions\DataFormats\selectorEncodeTabArrToFormat;
 use function Hexlet\Code\Functions\DataAggregate\tabDiff;
-use function Hexlet\Code\Functions\FS\getFilePathnameMainset;
-use function Hexlet\Code\Functions\FS\checkFileMainset;
+use function Hexlet\Code\Functions\FS\setFileModel;
+use function Hexlet\Code\Functions\FS\getFilePathnameSet;
+use function Hexlet\Code\Functions\FS\checkFile__model;
+use function Hexlet\Code\Functions\FS\permitFileExtension__model;
 
 /**
- * Функция-скрипт (функция-скрипт, функция-проект)
+ * Функция-скрипт (функция-скрипт, функция-проект, модуль)
  * Структура подобие скрипт:
  * - Аргументы как у скрипта, определяют внутренние опции
  * - кроме выполнения, те не содержит Composer autoload подключение
  * - кроме docopt
  * - кроме Характеристики проект-задача, входящие данные FS: процесс-получение
  */
-function gendiff($file1_pathname, $file2_pathname, $format = 'string')
+function gendiff($file1Pathname, $file2Pathname, $format = 'string')
 {
-    ### Задача-часть. Данные FS: характеристики Путь абс
-    $file1_pathname = getFilePathnameMainset($file1_pathname);
-    $file2_pathname = getFilePathnameMainset($file2_pathname);
-    // $format = $format ?? 'stylish'; // разрабатывается далее
+    ### Задача-часть. Модуль характеристики (Настройка)
+    $settingExtensions = ['yaml', 'yml', 'json'];
+    $settingFormat = ['string'];
 
-    ### Задача-часть. Данные FS: процесс-проверка
-    checkFileMainset($file1_pathname);
-    checkFileMainset($file2_pathname);
+    ### Задача-часть. Данные-модель характеристики (сущности-модели)
+        // - характеристики Путь абс: получение
+        // - создание моделей с характеристиками
+    $filesPathname = [$file1Pathname, $file2Pathname];
+    $files = array_map(function ($pathname) {
+        $filePathname__abs = getFilePathnameSet($pathname);
+
+        return setFileModel($filePathname__abs);
+    }, $filesPathname);
+
+    ### Задача-часть. Данные: процесс-проверка FS
+    array_map(function ($file) {
+        checkFile__model($file);
+
+        return $file;
+    }, $files);
+
+    ### Задача-часть. Модуль характеристики: проверка-разрешение расширение файла
+    array_map(function ($file) use ($settingExtensions) {
+        permitFileExtension__model($settingExtensions, $file);
+
+        return $file;
+    }, $files);
 
     ### Задача-часть. Данные обработка: сравнение совокупности характеристик (обработка, diff) двух файлов
+    // $format = $format ?? 'stylish'; // разрабатывается далее
     // Результат (данные исходящие): формат arr
-    $gendiff_result = tabDiff($file1_pathname, $file2_pathname);
+
+    $file1Pathname = $files['0']['pathname'];
+    $file2Pathname = $files['1']['pathname'];
+
+    $gendiff_result = tabDiff($file1Pathname, $file2Pathname);
 
     ### Задача-часть. Данные обработка: формат strings
     $gendiff_result__formatString = selectorEncodeTabArrToFormat(
         $tabArr = $gendiff_result,
         $format = 'string',
-        $brackets = '[]',
+        $brackets = '{}',
         $funcItem = function ($item) {
             return $item['status'] . ' ' . $item['name'] . ': ' . $item['value'];
         }
